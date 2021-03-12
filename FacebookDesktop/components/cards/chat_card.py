2bee00a12756158df37e57c880b634abdba76696
macbookair <kivydevelopment@gmail.com>
2021-03-12 10:30:34 +0300

    Added the ability to change the cursor type when hovering over a card element

--- FacebookDesktop/components/cards/chat_card.py
+++ FacebookDesktop/components/cards/chat_card.py
@@ -1,3 +1,4 @@
+from kivy.core.window import Window
 from kivy.properties import StringProperty
 
 from FacebookDesktop.components.cards.fake_card import FakeCard
@@ -7,3 +8,9 @@ class ChatCard(FakeCard):
     avatar = StringProperty()
     text = StringProperty()
     name = StringProperty()
+
+    def on_enter(self):
+        Window.set_system_cursor("hand")
+
+    def on_leave(self):
+        Window.set_system_cursor("arrow")
